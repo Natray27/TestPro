e8640379c6d5e5c732808cfc22aba9e10f73663a
ray <natray27@163.com>
2015-12-29 15:28:12 +0800

    addPro

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Ray on 15/12/1.
+//  Copyright © 2015年 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Ray on 15/12/1.
+//  Copyright © 2015年 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
